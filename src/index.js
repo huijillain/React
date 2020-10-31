222a9997e271ec9d08fe7e8a63122f8608523c13
Sky <skyjillain@gmail.com>
2020-10-30 19:52:09 -0600

    create components

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ let skiData = {
     goal: 100
 }
 
+const getPercent = decimal => {
+    return decimal * 100 + '%'
+}
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total/goal)
+}
+
 const SkiDayCounter = ({total, powder, backcountry, goal}) => {
     return (
         <section>
@@ -21,39 +28,11 @@ const SkiDayCounter = ({total, powder, backcountry, goal}) => {
                    <p>Backcountry Days: {backcountry}</p>
                </div>
                <div>
-                   <p>Goal Progress: {goal}</p>
+                   <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
                </div>
             </section>
     )
 }
-
-// class SkiDayCounter extends Component {
-//     getPercent = decimal => {
-//         return decimal * 100 + '%'
-//     }
-//     calcGoalProgress = (total, goal) => {
-//         return this.getPercent(total/goal)
-//     }
-//     render() {
-//         const {total, powder, backcountry, goal} = this.props
-//         return (
-//             <section>
-//                <div>
-//                    <p>Total Days: {total}</p>
-//                </div>
-//                <div>
-//                    <p>Powder Days: {powder}</p>
-//                </div>
-//                <div>
-//                    <p>Backcountry Days: {backcountry}</p>
-//                </div>
-//                <div>
-//                    <p>Goal Progress: {this.calcGoalProgress(total,goal)}</p>
-//                </div>
-//             </section>
-//         )
-//     }
-// }
 
 render(
     <SkiDayCounter 
