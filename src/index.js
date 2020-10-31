c6190b5abbdbbe9977035874ee2e61a621c041a8
Sky <skyjillain@gmail.com>
2020-10-30 19:36:42 -0600

    import changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component} from 'react'
+import { render} from 'react-dom'
 
 let skiData = {
     total: 50,
@@ -8,28 +8,29 @@ let skiData = {
     goal: 100
 }
 
-class SkiDayCounter extends React.Component {
+class SkiDayCounter extends Component {
     render() {
+        const {total, powder, backcountry, goal} = this.props
         return (
             <section>
                <div>
-                   <p>Total Days: {this.props.total}</p>
+                   <p>Total Days: {total}</p>
                </div>
                <div>
-                   <p>Powder Days: {this.props.powder}</p>
+                   <p>Powder Days: {powder}</p>
                </div>
                <div>
-                   <p>Backcountry Days: {this.props.backcountry}</p>
+                   <p>Backcountry Days: {backcountry}</p>
                </div>
                <div>
-                   <p>Goal: {this.props.goal}</p>
+                   <p>Goal: {goal}</p>
                </div>
             </section>
         )
     }
 }
 
-ReactDOM.render(
+render(
     <SkiDayCounter 
     total={skiData.total}
     powder={skiData.powder}
