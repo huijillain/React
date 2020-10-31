eca21c6fc9d8ce5ee786bad630697002dfc6565b
Sky <skyjillain@gmail.com>
2020-10-30 19:32:04 -0600

    skiData

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Message extends React.Component {
+let skiData = {
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100
+}
+
+class SkiDayCounter extends React.Component {
     render() {
-        console.log(this.props)
         return (
-            <div>
-                <h1 style={{color: this.props.color}}>
-                    {this.props.msg}</h1>
-                    <p>I'll check back in {this.props.minutes} minutes</p>
-            </div>
+            <section>
+               <div>
+                   <p>Total Days: {this.props.total}</p>
+               </div>
+               <div>
+                   <p>Powder Days: {this.props.powder}</p>
+               </div>
+               <div>
+                   <p>Backcountry Days: {this.props.backcountry}</p>
+               </div>
+               <div>
+                   <p>Goal: {this.props.goal}</p>
+               </div>
+            </section>
         )
     }
 }
 
-ReactDOM.render(<Message color="blue" msg="How are you?" minutes={5}/>, document.getElementById('root'))
+ReactDOM.render(
+    <SkiDayCounter 
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}/>,
+document.getElementById('root'))
