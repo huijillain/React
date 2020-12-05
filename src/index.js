ddd4e18d429481d7d63e949e489eda3b1a8f66e6
Sky <skyjillain@gmail.com>
2020-12-05 14:59:17 -0700

    Refactoring elements using JSX

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 ReactDOM.render(
-  React.createElement("h1", null, "Heyyyyy Everyone!"),
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+  </ul>,
   document.getElementById("root")
 );
